24089605426c7e8a1a7d26f38360b55bfa23f7a8
Riesedust <lucianoleon2003@gmail.com>
2021-12-13 18:27:41 -0300

    nodemon

--- api/index.js
+++ api/index.js
@@ -20,9 +20,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {preTemperamentos}= require('./src/controllers/temperamentoController')
+const port = process.env.PORT || 3001
+
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(5432 || 3001, async () => {
+conn.sync({ force: false }).then(() => {
+  server.listen(port, async () => {
     const preload = await preTemperamentos()
     console.log('%s ' + preload)
     console.log('%s listening at 3001'); // eslint-disable-line no-console
